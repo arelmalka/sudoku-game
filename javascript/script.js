b074fbcd635bd46cbf3681d999c813ade2a54457
arel malka <75252657+arelmalka@users.noreply.github.com>
2021-07-19 19:16:16 +0300

    Update script.js

--- javascript/script.js
+++ javascript/script.js
@@ -1,7 +1,7 @@
 function startGame() {
     let userName = document.getElementById('fullName').value;
     let password = document.getElementById('password').value;
-    
+    debugger;
     if (userName == 'abcd' && password == '1234') {
         window.location.replace('./gamePage.html');
     }
